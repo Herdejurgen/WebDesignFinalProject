b86c3b6c181f0c3dc4f0bb1ecdc8ed12847a6a2b
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-12-15 15:07:28 -0600

    remove id check edit

--- add.php
+++ add.php
@@ -62,10 +62,6 @@
       die("Connection failed: " . $conn->connect_error);
     }
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if($_POST['ID'] != null){
-
-            //$sql = ;
-            //echo $sql;
 
             $ID = $_POST["ID"];
             $Name = $_POST["Name"];
@@ -82,8 +78,7 @@
             $stmt->bind_param('isiiiiiiss',  $ID, $Name, $HP, $Atk, $Def, $SpAtk, $SpDef, $Speed, $Type1, $Type2);
 
             $stmt->execute();
-            printf("%d row inserted.\n", $stmt->affected_rows);
-        }
+            printf("%d row inserted.\n", $stmt->affected_rows);        
     }
     ?>
 
