b9b73b32c7a8bed7f6b48d3af3fedcbc95ce5fc0
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-12-15 16:05:25 -0600

    delete page functinality

--- delete.php
+++ delete.php
@@ -25,7 +25,17 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {        
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+        $ID = $_POST["ID"];
+
+        $sql = "DELETE FROM Pokemon WHERE id = $ID"
+
+        if ($conn->query($sql) === TRUE) {
+          echo "Record deleted successfully";
+        } else {
+          echo "Error deleting record: " . $conn->error;
+        }
     }
     ?>
 
