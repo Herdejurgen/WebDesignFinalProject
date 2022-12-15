c417516a425e9b55614c1963ed1be44f64e8e0bb
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-12-15 15:37:17 -0600

    change assignmenet edit

--- edit.php
+++ edit.php
@@ -63,9 +63,18 @@
     }   
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-            $ID = $_POST["ID"];            
+            $ID = $_POST["ID"];
+            $Name = $_POST["Name"];
+            $HP = $_POST["HP"];
+            $Atk = $_POST["Atk"];
+            $Def = $_POST["Def"];
+            $SpAtk = $_POST["SpAtk"];
+            $SpDef = $_POST["SpDef"];
+            $Speed = $_POST["Speed"];
+            $Type1 = $_POST["Type1"];
+            $Type2 = $_POST["Type2"];            
 
-            $sql = "UPDATE Pokemon SET Name = $_POST["Name"], HP = $_POST["HP"], Atk = $_POST["Atk"], Def = $_POST["Def"], SpAtk = $_POST["SpAtk"], SpDef = $_POST["SpDef"], Speed = $_POST["Speed"], Type1 = $_POST["Type1"], Type2 = $_POST["Type2"] WHERE ID = $_POST["ID"]";
+            $sql = "UPDATE Pokemon SET Name = $Name, HP = $HP, Atk = $Atk, Def = $Def, SpAtk = $SpAtk, SpDef = $SpDef, Speed = $Speed, Type1 = $Type1, Type2 = $Type2 WHERE ID = $_POST["ID"]";
 
             $stmt = $conn->prepare($sql);
             $stmt->execute();        
