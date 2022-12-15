a762fb0f67e70dfe9ae20bbcc9824bdc936236a5
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-12-15 11:38:59 -0600

    work on add page

--- add.php
+++ add.php
@@ -8,14 +8,38 @@
   </head>
   <body>
     <h1>Add Pokemon</h1>
-<form method="post" action="add.php">
-  <div class="mb-3">
-    <label for="Name" class="form-label">Name</label>
-    <input type="text" class="form-control" id="Name" name="Name">
-    <div id="nameHelp" class="form-text">Enter the pokemon's name.</div>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+    <form method="post" action="add.php">
+      <div class="mb-3">
+        <label for="Name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="Name" name="Name">
+        <div id="nameHelp" class="form-text">Enter the pokemon's name.</div>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <?php
+    $servername = "localhost";
+    $username = "herdeju1_FinalProject";
+    $password = "U\$gvulNHsye?";
+    $dbname = "herdeju1_FinalProject";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    if($_POST['Name'] != null){
+    $Name = $_POST['Name'];
+
+    $sql = "insert into Pokemon (Name) value (?)";
+    //echo $sql;
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("Name", $Name);
+        $stmt->execute();
+    }
+    
+    ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </body>
 </html>
