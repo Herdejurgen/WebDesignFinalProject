3a7afbd3853a9b5727e3dd7e4295fdf7790d7183
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-12-15 12:13:46 -0600

    variables

--- add.php
+++ add.php
@@ -53,8 +53,16 @@
     if ($conn->connect_error) {
       die("Connection failed: " . $conn->connect_error);
     }
-    if($_POST['Name'] != null){
+    if($_POST['ID'] != null){
+    $ID = $_POST['ID'];
     $Name = $_POST['Name'];
+    $Atk = $_POST['Atk'];
+    $Def = $_POST['Def'];
+    $SpAtk = $_POST['SpAtk'];
+    $SpDef = $_POST['SpDef'];
+    $Speed = $_POST['Speed'];
+    $Type1 = $_POST['Type1'];
+    $Type2 = $_POST['Type2'];
 
     $sql = "INSERT INTO Pokemon values (?,?,?,?,?,?,?,?,?,?)";
     //echo $sql;
@@ -62,6 +70,7 @@
         $stmt->bind_param("isiiiiiiss", $ID, $Name, $HP, $Atk, $Def, $SpAtk, $SpDef, $Speed, $Type1, $Type2);
 
         $stmt->execute();
+        printf("%d row inserted.\n", $stmt->affected_rows);
     }
     
     ?>
